a4e00bbb2c2d85492d66ef8be738c6b022e9fb56
Mike Brevoort <mike.brevoort@pearson.com>
2014-02-02 21:27:54 -0700

    fixed (?) sträva iframe insecure css issue

--- routes/strava.js
+++ routes/strava.js
@@ -3,6 +3,7 @@ var request = require('request')
   ;
 
 var relativeRe = new RegExp('href="/athletes/', 'g');
+var protocolRe = new RegExp('http://', 'g');
 
 exports.latest = function(req, response, next) {
   request({
@@ -11,8 +12,8 @@ exports.latest = function(req, response, next) {
     if (res.headers['content-type']){
       response.setHeader('content-type', res.headers['content-type']);
     }
-    var re = new RegExp('href="/athletes/', 'g');
     body = body.replace(relativeRe, 'href="http://www.strava.com/athletes/');
+    body = body.replace(protocolRe, '//');
     response.send(body);
   })
   .on('error', function (error) {
@@ -22,12 +23,16 @@ exports.latest = function(req, response, next) {
 }
 
 exports.summary = function(req, response, next) {
-  http.get({
-    host: 'www.strava.com',
-    path: '/clubs/rocktriclub/latest-rides/cc320539735d33d930ac41e763b73b7ac6d65925?show_rides=false'
-  }, function(res) {
-    res.pipe(response);
-  }).on('error', function (error) {
+  request({
+    uri: 'http://www.strava.com/clubs/rocktriclub/latest-rides/cc320539735d33d930ac41e763b73b7ac6d65925?show_rides=false'
+  }, function (error, res, body) {
+    if (res.headers['content-type']){
+      response.setHeader('content-type', res.headers['content-type']);
+    }
+    body = body.replace(protocolRe, '//');
+    response.send(body);
+  })
+  .on('error', function (error) {
     console.log('strava latest error:', error);
     response.send(error);
   });
